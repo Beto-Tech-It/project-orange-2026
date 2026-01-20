c9144d5caf4ae198ecc357abeaaa8d7bf43d6ce0
Humberto Palomino <beto.tech.it@gmail.com>
2026-01-19 21:40:52 -0500

    Ingresar nuevos cambios

--- cypress/e2e/web/pages/login/login.page.ts
+++ cypress/e2e/web/pages/login/login.page.ts
@@ -6,17 +6,26 @@ export class LoginPage {
    ingresarCredenciales(username: string, password: string) {
     cy.fixture("login").then((elements) => {
         // Solo escribir username si NO está vacío
-        if (username !== '') {
+
+        if(username === 'vacio'){
+            username = '';
+        } else {
             cy.get(elements.login_user).type(username);
         }
-        
-        // Solo escribir password si NO está vacío
-        if (password !== '') {
+
+        if(password === 'vacio'){
+            password = '';
+        } else {
             cy.get(elements.psw_user).type(password);
         }
-        
-        // Siempre hacer click en el botón
         cy.get(elements.button_click).click().wait(2000);
+
+
+        // const user = username === 'vacio' ? '' : username; // Convertir "vacio" a string vacío - operador ternario
+        // const pass = password === 'vacio' ? '' : password; // Convertir "vacio" a string vacío - operador ternario
+       
+        // Siempre hacer click en el botón
+        
     });
 }
 }
